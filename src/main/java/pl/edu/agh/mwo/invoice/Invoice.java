6e967ebf956c0de442d91c059092aa8c59bc7381
pawelkawalec <pawel.kawalec@interia.pl>
2024-06-13 11:16:08 +0200

    implemented all methods in class 'Invoice' except '.getNumber()'

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,54 +1,115 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Map<Product, Integer> products = new HashMap<Product, Integer>();
-
-    private int number = 1;
+    private Map<Product, Integer> products = new HashMap<>();
+    private Collection<Product> products2 = new ArrayList<>();
 
     public void addProduct(Product product) {
-        addProduct(product, 1);
+        if (product == null) {
+            throw new IllegalArgumentException("Null product");
+        }
+        this.products2.add(product);
+        // TODO: implement
     }
 
     public void addProduct(Product product, Integer quantity) {
-        if (product == null || quantity <= 0) {
-            throw new IllegalArgumentException();
+        if (quantity <= 0 || product == null) {
+            throw new IllegalArgumentException("Invalid quantity or null product");
         }
-        products.put(product, quantity);
+        this.products.put(product, products.getOrDefault(product, 0) + quantity);
+        //this.products2.add(product);
+        // TODO: implement
     }
 
     public BigDecimal getNetTotal() {
-        BigDecimal totalNet = BigDecimal.ZERO;
-        for (Product product : products.keySet()) {
-            BigDecimal quantity = new BigDecimal(products.get(product));
-            totalNet = totalNet.add(product.getPrice().multiply(quantity));
+
+        BigDecimal netPrice = BigDecimal.ZERO;
+        for (Product product : this.products2) {
+            netPrice = netPrice.add(product.getPrice());
         }
-        return totalNet;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            netPrice = netPrice.add(product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+        }
+        return netPrice;
+
     }
 
     public BigDecimal getTaxTotal() {
-        return getGrossTotal().subtract(getNetTotal());
+
+
+        BigDecimal totalTax = BigDecimal.ZERO;
+
+        for (Product product : products2) {
+            BigDecimal tax = product.getPrice().multiply(product.getTaxPercent());
+            totalTax = totalTax.add(tax);
+        }
+
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+
+            BigDecimal tax = product.getPrice().multiply(product.getTaxPercent()).multiply(BigDecimal.valueOf(quantity));
+            totalTax = totalTax.add(tax);
+        }
+
+        return totalTax.setScale(2, BigDecimal.ROUND_HALF_UP);
+
+    }
+
+    public BigDecimal getGrossPrice() {
+
+
+        BigDecimal totalGrossPrice = BigDecimal.ZERO;
+
+
+        for (Product product : products2) {
+            BigDecimal grossPrice = product.getPrice().add(product.getPrice().multiply(product.getTaxPercent()));
+            totalGrossPrice = totalGrossPrice.add(grossPrice);
+        }
+
+
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+            BigDecimal grossPrice = product.getPrice().add(product.getPrice().multiply(product.getTaxPercent()));
+
+            grossPrice = grossPrice.multiply(BigDecimal.valueOf(quantity));
+            totalGrossPrice = totalGrossPrice.add(grossPrice);
+        }
+
+        return totalGrossPrice.setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public BigDecimal getSubtotal() {
+
+        BigDecimal subtotal = BigDecimal.ZERO;
+        for (Product product : products2) {
+            subtotal = subtotal.add(product.getPrice());
+        }
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            subtotal = subtotal.add(product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+        }
+        return subtotal;
+
     }
 
     public BigDecimal getGrossTotal() {
-        BigDecimal totalGross = BigDecimal.ZERO;
-        for (Product product : products.keySet()) {
-            BigDecimal quantity = new BigDecimal(products.get(product));
-            totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
-        }
-        return totalGross;
+        return getGrossPrice();
     }
 
-    public int getNumber() {
-        return number;
-    }
-
-    public Map<Product, Integer> getProducts() {
-        return products;
-    }
+    //public int getNumber() {
+   // }
 }
